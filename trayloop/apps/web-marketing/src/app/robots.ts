ed6239bb86a555356267281259f15674a0218e6b
Sdotts <166665964+SimonShaun2@users.noreply.github.com>
2026-04-14 17:47:32 -0500

    SEO: update robots.ts with disallow rules and host directive

--- trayloop/apps/web-marketing/src/app/robots.ts
+++ trayloop/apps/web-marketing/src/app/robots.ts
@@ -1,13 +1,15 @@
 import type { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
-  return {
-    rules: [
-      {
-        userAgent: '*',
-        allow: '/',
-      },
-    ],
-    sitemap: 'https://trayloophq.com/sitemap.xml',
-  };
+    return {
+          rules: [
+            {
+                      userAgent: '*',
+                      allow: '/',
+                      disallow: ['/api/', '/_next/'],
+            },
+                ],
+          sitemap: 'https://trayloophq.com/sitemap.xml',
+          host: 'https://trayloophq.com',
+    };
 }
